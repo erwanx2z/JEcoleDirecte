65373fb48ab0702cc25c4e6c5e3972afaa95ab4a
Ariloxe <ariloxegaming@gmail.com>
2022-02-16 15:44:09 +0100

    remove useless imports

--- src/main/java/fr/erwanx2z/api/impl/data/IGradeLoader.java
+++ src/main/java/fr/erwanx2z/api/impl/data/IGradeLoader.java
@@ -2,17 +2,6 @@ package fr.erwanx2z.api.impl.data;
 
 import fr.erwanx2z.api.impl.IProfile;
 import fr.erwanx2z.api.impl.data.grades.IPeriodeData;
-import fr.erwanx2z.core.impl.Profile;
-import fr.erwanx2z.core.impl.data.grades.PeriodeData;
-import fr.erwanx2z.core.impl.data.grades.SubjectData;
-import fr.erwanx2z.core.utils.EcoleDirecteLink;
-import fr.erwanx2z.core.utils.RequestUtils;
-import lombok.Getter;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Erwan - 2nde5
